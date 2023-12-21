7020ac927b7e52ebea53e36c13343719e11a0bac
Prateek-5 <prateek.ku11@gmail.com>
2023-12-21 20:46:39 +0530

    complet notes of lecture 3

--- Intro-Modules/6.ToBoolean-Equality.js
+++ Intro-Modules/6.ToBoolean-Equality.js
@@ -34,4 +34,64 @@ console.log(!console.log(!0));
     print as true only because the inner console.log will return underfined as its return type 
 */
 
+/*
+    7.2.14Abstract Equality Comparison
+        The comparison x == y, where x and y are values, produces true or false. Such a comparison is performed as follows:
 
+            If Type(x) is the same as Type(y), then
+            Return the result of performing Strict Equality Comparison x === y.  Look very closely at this point
+            If x is null and y is undefined, return true.
+            If x is undefined and y is null, return true.
+            If Type(x) is Number and Type(y) is String, return the result of the comparison x == ! ToNumber(y).
+            If Type(x) is String and Type(y) is Number, return the result of the comparison ! ToNumber(x) == y.
+            If Type(x) is Boolean, return the result of the comparison ! ToNumber(x) == y.
+            If Type(y) is Boolean, return the result of the comparison x == ! ToNumber(y).
+            If Type(x) is either String, Number, or Symbol and Type(y) is Object, return the result of the comparison x == ToPrimitive(y).
+            If Type(x) is Object and Type(y) is either String, Number, or Symbol, return the result of the comparison ToPrimitive(x) == y.
+            Return false.
+
+
+    7.2.15Strict Equality Comparison
+            The comparison x === y, where x and y are values, produces true or false. Such a comparison is performed as follows:
+
+                If Type(x) is different from Type(y), return false.
+                If Type(x) is Number, then
+                If x is NaN, return false.
+                If y is NaN, return false.
+                If x is the same Number value as y, return true.
+                If x is +0 and y is -0, return true.
+                If x is -0 and y is +0, return true.
+                Return false.
+                Return SameValueNonNumber(x, y).
+
+
+        Abstract Equality does coercion if types are not same strict equality never does coercion
+        7.2.12SameValueNonNumber ( x, y )
+                The internal comparison abstract operation SameValueNonNumber(x, y), where neither x nor y are Number values, 
+                produces true or false. Such a comparison is performed as follows:
+
+                Assert: Type(x) is not Number.
+                    Assert: Type(x) is the same as Type(y).
+                    If Type(x) is Undefined, return true.
+                    If Type(x) is Null, return true.
+                    If Type(x) is String, then
+                    If x and y are exactly the same sequence of code units (same length and same code units at corresponding indices), return true; otherwise, return false.
+                    If Type(x) is Boolean, then
+                    If x and y are both true or both false, return true; otherwise, return false.
+                    If Type(x) is Symbol, then
+                    If x and y are both the same Symbol value, return true; otherwise, return false.
+                    If x and y are the same Object value, return true. Otherwise, return false.
+
+
+
+ */
+
+console.log(NaN===NaN); //op false in JS NaN is the only operent which is not equal to itself
+
+//console.log({a:7}==={a:7}); //because the memory location is different return false
+x={a:7};
+
+console.log(x===x); //op true
+console.log(Symbol("prateek")===Symbol("prateek")); //false
+y=Symbol("prateek")
+console.log(y===y); //true
